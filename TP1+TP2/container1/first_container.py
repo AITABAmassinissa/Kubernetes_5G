0578d179c396018504769768e81ee0ee756714fa
massi <massinissa.aitaba@gmail.com>
2025-03-27 16:35:10 +0100

    p

---
+++ TP1+TP2/container1/first_container.py
@@ -0,0 +1,18 @@
+import numpy as np
+from time import sleep
+# Creation d'un tableau numpy
+arr = np.array([1, 2, 3, 4, 5])
+
+# Affichage du tableau
+print("Tableau numpy:", arr)
+
+# Affichage de la version de numpy
+print("Version de numpy:", np.__version__)
+
+sleep(10000)
+
+
+
+
+
+
